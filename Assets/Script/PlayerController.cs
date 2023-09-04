d2f23b2f9f0ef5c4b5b7228e4dab9e95903549e5
Argonaut <85242295+jeonghyeok-Lee@users.noreply.github.com>
2023-09-04 15:14:31 +0900

    refactor: PlayerController 리팩토링

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -4,51 +4,47 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] private float moveSpeed;
+    private Rigidbody playerRigidbody; // 케릭터 리지드바디
 
-    private float moveSpeed = 5.0f;
-    private Vector3 move;
-    private CharacterController characterController;
-    private Vector3 characterForward;                                               // 플레이어의 forward;
-    private Vector3 characterRight;                                                 // 플레이어의 right;
     private RotateToMouse rotateToMouse;                                            // 마우스 이동으로 카메라 회전
 
     private void Awake()
     {
-        characterController = GetComponent<CharacterController>();
         rotateToMouse = GetComponent<RotateToMouse>();
 
-        //초기화
-        characterForward = transform.forward;
-        characterRight = transform.right;
+        playerRigidbody = GetComponent<Rigidbody>();
     }
 
 
     // Update is called once per frame
     void Update()
     {
-        // WASD 입력 받기
-        // GetAxis (보간 사용) -> GetAxisRaw(보간 미사용) - 키 때는 즉시 종료
-        float moveX = Input.GetAxisRaw("Horizontal");   
-        float moveZ = Input.GetAxisRaw("Vertical");
-
-        characterForward = transform.forward;
-        characterRight = transform.right;
-        characterForward.Normalize();
-        characterRight.Normalize();
-
-        // 이동 방향 계산
-        move = characterForward * moveZ + characterRight * moveX;
-        move.Normalize();
-
-        // 캐릭터 이동
-        characterController.Move(move * moveSpeed * Time.deltaTime);
+        Move();
 
         // 마우스 좌클릭하며 마우스 이동시 카메라 화면 전환
-        if (Input.GetMouseButton(0))
-        {
-            float mouseX = Input.GetAxis("Mouse X");
-            float mouseY = Input.GetAxis("Mouse Y");
-            rotateToMouse.CalculateRotation(mouseX, mouseY);
+        if (Input.GetMouseButton(0)){
+            CameraRotation();
         }
+    }
+
+    private void Move(){
+        // 케릭터 이동
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveZ = Input.GetAxisRaw("Vertical");
+
+        Vector3 moveHorizontal = transform.right * moveX;
+        Vector3 moveVertical = transform.forward * moveZ;
+
+        Vector3 velocity = (moveHorizontal + moveVertical).normalized * moveSpeed;
+        
+        playerRigidbody.MovePosition(transform.position + velocity * Time.deltaTime);
+    }
+
+    private void CameraRotation(){
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+        
+        rotateToMouse.CalculateRotation(mouseX, mouseY);
     }
 }
