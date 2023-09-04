9e8fc807183050861563e595f50da38a8bbbc03b
Argonaut <85242295+jeonghyeok-Lee@users.noreply.github.com>
2023-09-04 21:50:42 +0900

    refactor: 마우스, 카메라 제어 기능 리팩토링
    CameraController 추가
    기존 RotateToMouse 함수 제거

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -4,16 +4,17 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed;
-    private Rigidbody playerRigidbody; // 케릭터 리지드바디
+    [SerializeField] private float moveSpeed;           // 케릭터 이동속도
+    [SerializeField] private float mouseSensitivity;    // 마우스 감도
 
-    private RotateToMouse rotateToMouse;                                            // 마우스 이동으로 카메라 회전
+    private Rigidbody playerRigidbody;          // 케릭터 리지드바디
+    private ICamUpdatable camUpdatable;         // 카메라 제어 인터페이스
 
     private void Awake()
     {
-        rotateToMouse = GetComponent<RotateToMouse>();
-
-        playerRigidbody = GetComponent<Rigidbody>();
+        playerRigidbody = GetComponent<Rigidbody>();            // 케릭터 리지드바디 컴포넌트 가져오기
+        
+        camUpdatable = GetComponentInChildren<ICamUpdatable>(); // 카메라 제어 인터페이스 컴포넌트 가져오기
     }
 
 
@@ -23,28 +24,26 @@ public class PlayerController : MonoBehaviour
         Move();
 
         // 마우스 좌클릭하며 마우스 이동시 카메라 화면 전환
-        if (Input.GetMouseButton(0)){
-            CameraRotation();
+        if (Input.GetMouseButton(0))
+        {
+            camUpdatable.UpdateRotation(gameObject, mouseSensitivity);
         }
     }
 
-    private void Move(){
-        // 케릭터 이동
+    private void Move()
+    {
+        // 케릭터 이동키 입력   
         float moveX = Input.GetAxisRaw("Horizontal");
         float moveZ = Input.GetAxisRaw("Vertical");
 
+        // 이동 값 계산
         Vector3 moveHorizontal = transform.right * moveX;
         Vector3 moveVertical = transform.forward * moveZ;
 
+        // 이동 값 정규화
         Vector3 velocity = (moveHorizontal + moveVertical).normalized * moveSpeed;
-        
-        playerRigidbody.MovePosition(transform.position + velocity * Time.deltaTime);
-    }
 
-    private void CameraRotation(){
-        float mouseX = Input.GetAxis("Mouse X");
-        float mouseY = Input.GetAxis("Mouse Y");
-        
-        rotateToMouse.CalculateRotation(mouseX, mouseY);
+        // 리지드바디를 이용한 이동
+        playerRigidbody.MovePosition(transform.position + velocity * Time.deltaTime);
     }
 }
