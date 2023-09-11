44ce3dd6c19523e204c6cd61dcb160a825d03296
Argonaut <85242295+jeonghyeok-Lee@users.noreply.github.com>
2023-09-11 23:38:22 +0900

    feat: 세분화에 따른 Point 클래스 추가

---
+++ Assets/Script/Point.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Point : MonoBehaviour
+{
+    public int pointNum;            // 포인트 번호
+    public bool isMiddle;           // 포인트가 중심점인지
+    public bool isShow;             // 포인트가 활성화 되었는지
+    public Transform pointTrans;    // 포인트의 Transform
+}
