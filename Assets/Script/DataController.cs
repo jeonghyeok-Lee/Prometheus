2d227676e2cc5bf91998d31001c6cb2aea05ac52
Argonaut <85242295+jeonghyeok-Lee@users.noreply.github.com>
2023-09-11 23:37:35 +0900

    refactor: 세분화를 위한 클래스 도출

---
+++ Assets/Script/DataController.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DataSetting{
+    [System.Serializable]
+    public struct ScanData{
+        public string angle;
+        public string distance;
+    }
+
+    [System.Serializable]
+    public struct ScanDataArray{
+        public ScanData[][] data;
+    }
+
+    /// <summary>
+    /// 데이터 관리 클래스
+    /// </summary>
+    public class DataController : MonoBehaviour
+    {
+        // private string type;        // 데이터 타입
+        // private string path;        // 데이터 경로
+        // private DataJson json;      // json 데이터
+
+        // public string Type{
+        //     get { return type; }
+        //     set { type = value; }
+        // }
+
+        // public string Path{
+        //     get { return path; }
+        //     set { path = value; }
+        // }
+
+        // public DataController(){
+        //     type = "";
+        //     path = "";
+        // }
+
+        // public DataController(string type, string path){
+        //     this.type = type;
+        //     this.path = path;
+        // }
+
+
+        // public ScanDataArray LoadData(){
+        //     if(path != ""){
+        //         switch(type){
+        //             case "json":
+        //                 json = new DataJson(path);
+        //                 break;
+        //             default:
+        //                 break;
+        //         }
+        //         return;
+        //     }
+        //     return null;
+        // }
+
+        // 데이터 로드
+        // data 저장
+        // 데이터 수집
+        // 데이터 관리
+    }
+
+}
+
