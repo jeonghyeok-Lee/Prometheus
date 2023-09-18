2c3f9ab547effce73a076bda48820041836dc1aa
Argonaut <85242295+jeonghyeok-Lee@users.noreply.github.com>
2023-09-18 12:06:51 +0900

    feat: 같은 포인터끼리 충돌 시 오브젝트 파괴 기능 추가

--- Assets/Script/Point.cs
+++ Assets/Script/Point.cs
@@ -8,4 +8,13 @@ public class Point : MonoBehaviour
     public bool isMiddle;           // 포인트가 중심점인지
     public bool isShow;             // 포인트가 활성화 되었는지
     public Transform pointTrans;    // 포인트의 Transform
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Point")
+        {
+            // Debug.Log(other.gameObject.name + "와 충돌");
+            Destroy(other.gameObject);  
+        }
+    }
 }
