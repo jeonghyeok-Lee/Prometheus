f876d418ffe0ff294d9fa89d420059d0b5f30405
Argonaut <85242295+jeonghyeok-Lee@users.noreply.github.com>
2023-10-30 21:21:30 +0900

    test: 예제 테스트 코드 추가 및 테스트 코드 수정

--- Assets/Script/PointCloudController.cs
+++ Assets/Script/PointCloudController.cs
@@ -28,7 +28,7 @@ namespace testest{
 
         public Material pointCloudMaterial; // 포인트 클라우드를 렌더링할 Material
 
-        public float distanceRatio = 0.1f; // 포인트 클라우드의 거리 비율
+        public float distanceRatio = 0.01f; // 포인트 클라우드의 거리 비율
 
         void Start()
         {
@@ -66,15 +66,15 @@ namespace testest{
             meshRenderer.material = pointCloudMaterial;
         }
 
-        // 재작성 필요 현재 data의 값을 가져오지 못하고 있음..
         private void Test(){
 
             // 테스트용
             // PointDataTest json = JsonUtility.FromJson<PointDataTest>(jsonFile2.text);
             PointDataTest jsonTest = JsonConvert.DeserializeObject<PointDataTest>(jsonFile2.ToString());             // 스캔한 데이터
-            Debug.Log(jsonTest.depth_data.Length);
-            Debug.Log(jsonTest.depth_data[0].Length);
-            
+
+            int width = jsonTest.depth_data[0].Length;
+            int height = jsonTest.depth_data.Length;
+
             int number = 0;
 
             // 좌표로 변환
@@ -86,30 +86,32 @@ namespace testest{
 
             // 각 포인트에 대해 메시에 추가
             Mesh pointCloudMesh = new Mesh();
-            Vector3[] vertices = new Vector3[jsonTest.depth_data.Length];
+            int arraySize = width * height;
+            Vector3[] vertices = new Vector3[arraySize];
+            int vertexIndex = 0;
 
-            // 데이터가 배열의 배열이므로 두 번의 루프를 사용
-            for (int i = 0; i < jsonTest.depth_data.Length; i++)
+            for (int i = 0; i < height; i++)
             {
-                int[] row = jsonTest.depth_data[i];
-
-                for (int j = 0; j < row.Length; j++)
+                for (int j = 0; j < width; j++)
                 {
-                    float depthValue = row[j];  // 깊이 데이터
-                
-                    // 깊이를 통해 포인터들의 좌표를 계산
-                    Vector3 coordinate = CalPointPosAndRot(0, depthValue, pointCloudObject, number++);
+                    float depth = jsonTest.depth_data[i][j] * distanceRatio;
 
-                    vertices[i] = coordinate;   // 메시에 추가
+                    // 이미지의 중앙을 (0, 0, 0)으로 간주하고, 각 픽셀의 좌표를 계산
+                    float x = (j - width / 2) * distanceRatio;
+                    float y = (i - height / 2) * distanceRatio;
 
-                    // 계산된 좌표를 리스트에 추가
-                    unityCoordinates.Add(coordinate);
+                    // depth_data의 값이 이미지에서 해당 픽셀까지의 깊이를 나타내므로, z 좌표로 사용
+                    float z = depth;
 
+                    if(x != 0.0f || y != 0.0f || z != 0.0f){
+                        vertices[vertexIndex] = new Vector3(x, y, z);
+                        vertexIndex++;
+                    }
                 }
             }
 
             pointCloudMesh.vertices = vertices;
-            pointCloudMesh.SetIndices(Enumerable.Range(0, jsonTest.depth_data.Length).ToArray(), MeshTopology.Points, 0);
+            pointCloudMesh.SetIndices(Enumerable.Range(0, arraySize).ToArray(), MeshTopology.Points, 0);
 
             // 메시 필터 및 렌더러 추가
             MeshFilter meshFilter = pointCloudObject.AddComponent<MeshFilter>();
@@ -117,46 +119,8 @@ namespace testest{
 
             meshFilter.mesh = pointCloudMesh;
             meshRenderer.material = pointCloudMaterial;
-
-
-            // int count = 0;
-            // // 변환된 좌표 출력
-            // foreach (var coordinate in unityCoordinates)
-            // {
-            //     Debug.Log("좌표 : " + coordinate + " / " + count++ + "번째" + " / " + zeroCount);
-            // }
-
         }
-
-        /// <summary>
-        /// 포인트의 위치 및 회전값을 계산하는 메서드
-        /// </summary>
-        /// <param name="angle">중심점과의 각도 포인터가 위치할 각도</param>
-        /// <param name="distance">중심점과의 거리 포인터가 중심점으로부터 떨어진 거리</param>
-        private Vector3 CalPointPosAndRot(float angle, float distance, GameObject middle, int number)
-        {
-            // 중심점[플레이어]의 회전값을 변경
-            middle.transform.rotation = Quaternion.Euler(middle.transform.rotation.x, angle, middle.transform.rotation.z);
-
-            // 포인트컨트롤러가 바라보는 방향에서 0.01f만큼 떨어진 위치를 계산
-            Vector3 directionFromCenter = middle.transform.forward * distance * distanceRatio;
-
-            // number%320만큼 x좌표 값을 증가
-            directionFromCenter.x += (number % 320) * distanceRatio;
-            if(number / 320 > 0){
-                directionFromCenter.y += (number / 320) * distanceRatio;
-            }
-
-            // 포인트가 생성될 위치를 계산
-            Vector3 spanwPos = middle.transform.position + directionFromCenter;
-
-            return spanwPos;
-        }
-
-
-
-
-
+        
         /// <summary>
         /// //////////////////////////////// 테스트용
         /// </summary>
