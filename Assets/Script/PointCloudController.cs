db0fdc22cad63d9ce38e7b1c691c25cd691f88a1
Argonaut <85242295+jeonghyeok-Lee@users.noreply.github.com>
2023-11-05 23:51:49 +0900

    modify: 포인트 클라우드 출력 함수 변경
    기존 모양이 이상한 것 같아 수정 진행

--- Assets/Script/PointCloudController.cs
+++ Assets/Script/PointCloudController.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 using System.Linq;
 using Newtonsoft.Json;
 
-
 public class PointCloudController : MonoBehaviour
 {
     public TextAsset jsonFile; // JSON 파일을 할당하기 위한 변수
@@ -12,17 +11,65 @@ public class PointCloudController : MonoBehaviour
     public Material pointCloudMaterial; // 포인트 클라우드를 렌더링할 Material
 
     public float distanceRatio = 0.01f; // 포인트 클라우드의 거리 비율
+    public float depthScale = 0.01f; // 포인트의 깊이에 대한 스케일 조정
 
     void Start()
     {
-        Test();
-
-
+        GeneratePointCloud();
     }
 
+    private void GeneratePointCloud()
+    {
+        // JSON 파일 파싱
+        PointDataTest jsonData = JsonConvert.DeserializeObject<PointDataTest>(jsonFile.ToString());
+
+        int width = jsonData.depth_data[0].Length;
+        int height = jsonData.depth_data.Length;
+
+        // 포인트 클라우드 생성
+        GameObject pointCloudObject = new GameObject("PointCloud");
+        pointCloudObject.transform.position = new Vector3(0, 0, 10); // 카메라에서 10의 거리에 배치
+
+        Mesh pointCloudMesh = new Mesh();
+        int arraySize = width * height;
+        Vector3[] vertices = new Vector3[arraySize];
+        int vertexIndex = 0;
+
+        // Unity의 월드 좌표에서 이미지 중앙으로 이동하는 벡터
+        Vector3 offset = new Vector3(-width / 2 * distanceRatio, -height / 2 * distanceRatio, 0);
+
+        for (int i = 0; i < height; i++)
+        {
+            for (int j = 0; j < width; j++)
+            {
+                float depth = jsonData.depth_data[i][j] * distanceRatio;
+
+                float x = j * distanceRatio;
+                float y = (height - i) * distanceRatio;
+                float z = depth;
+
+                // Unity의 월드 좌표로 변환 후 offset 적용
+                Vector3 worldCoordinate = new Vector3(x, y, z) + offset;
+
+                vertices[vertexIndex] = worldCoordinate;
+                vertexIndex++;
+            }
+        }
+
+        pointCloudMesh.vertices = vertices;
+        pointCloudMesh.SetIndices(Enumerable.Range(0, arraySize).ToArray(), MeshTopology.Points, 0);
+
+        // MeshFilter 및 MeshRenderer 추가
+        MeshFilter meshFilter = pointCloudObject.AddComponent<MeshFilter>();
+        MeshRenderer meshRenderer = pointCloudObject.AddComponent<MeshRenderer>();
+
+        meshFilter.mesh = pointCloudMesh;
+        meshRenderer.material = pointCloudMaterial;
+    }
+
+    // 테스트용
     private void Test()
     {
-
         // 테스트용
         // PointDataTest json = JsonUtility.FromJson<PointDataTest>(jsonFile2.text);
         PointDataTest jsonTest = JsonConvert.DeserializeObject<PointDataTest>(jsonFile.ToString());             // 스캔한 데이터
