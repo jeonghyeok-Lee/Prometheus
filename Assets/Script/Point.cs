97da685bf76c00ac45c3a7a8e8c908e1207a67ca
Argonaut <85242295+jeonghyeok-Lee@users.noreply.github.com>
2023-11-13 16:12:25 +0900

    feat: 플레이어 제어 기능 추가

--- Assets/Script/Point.cs
+++ Assets/Script/Point.cs
@@ -5,18 +5,16 @@ using UnityEngine;
 public class Point : MonoBehaviour
 {
     public int pointNum;            // 포인트 번호
-    public float x;                 // 포인트의 x좌표
-    public float y;                 // 포인트의 y좌표
-    public float z;                 // 포인트의 z좌표
+    public bool isMiddle;           // 포인트가 중심점인지
     public bool isShow;             // 포인트가 활성화 되었는지
     public Transform pointTrans;    // 포인트의 Transform
 
-    /// <summary>
-    /// @todo : 생성된 포인트 위치에 다른 포인트가 있으면 생성하지 않도록 수정
-    /// </summary>
-    /// <param name="other"></param>
     private void OnTriggerEnter(Collider other)
     {
-
+        if(other.gameObject.tag == "Point")
+        {
+            // Debug.Log(other.gameObject.name + "와 충돌");
+            Destroy(other.gameObject);  
+        }
     }
 }
