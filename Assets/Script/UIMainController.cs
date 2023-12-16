5c5e622a38aab8a394e4e2584e93dcb079432dfb
Argonaut <85242295+jeonghyeok-Lee@users.noreply.github.com>
2023-12-17 03:05:26 +0900

    feat: 모드 변경에 따른 텍스트가 수정되는 기능 추가

--- Assets/Script/UIMainController.cs
+++ Assets/Script/UIMainController.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIMainController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    public Text modeText; // 현재 플레이어 모드 텍스트
+
+    public void SetModeText(string mode)
     {
-        
+        // 예외 처리: 텍스트 컴포넌트가 없는 경우 에러 출력
+        if (modeText == null)
+        {
+            Debug.LogError("텍스트 컴포넌트가 없음!");
+            return;
+        }
+
+        // 텍스트 컴포넌트의 텍스트 변경
+        modeText.text = mode;
     }
 }
